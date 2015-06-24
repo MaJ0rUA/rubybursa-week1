848b025566a3fb11c93e770b65691afdc6304d76
MaJ0rUA <lytvynal@gmail.com>
2015-06-24 16:48:28 +0300

    first commit

---
+++ author.rb
@@ -0,0 +1,9 @@
+class Author
+	def initialize(name, date_of_birth, date_of_death)
+		@name = name
+		@date_of_birth = date_of_birth
+		@date_of_death = date_of_death
+	end
+	my_author = Author.new("William Shakespeare", 1564, 1616)
+	end
+end	
